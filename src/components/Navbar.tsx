f8a8b67fb35ae89c053ca6d23d614d33c318c487
LoejeeMan <ldulaugon123@gmail.com>
2025-09-20 14:49:16 +0800

    feat: Added subscribe button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,13 @@ const Navbar: React.FC = () => {
   return (
     <header className="flex justify-between items-center mb-10">
       <img src={LogoCName} className="h-10" />
+      <a
+        href="https://forms.gle/aURM8uXKgkP8Xz2W6"
+        target="_blank"
+        className="bg-primary rounded-full px-4 py-2 text-white font-bold  hover:cursor-pointer hover:scale-120 transition"
+      >
+        Request A Song By Pre-ubcribing
+      </a>
     </header>
   );
 };
